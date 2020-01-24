5c15d4eea08c5060fa3b099d0c81dcf310275c30
Chayan Martinez <nayachmartinez68@gmail.com>
2020-01-24 10:40:37 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,11 +1,15 @@
 class Student < User
 
-def initialize
-  @knowledge = []
-end
-
-def learn(string)
-  @knowledge << (string).to_s
-end  
+  def initialize
+    @knowledge = []
+  end
+  
+  def learn(string)
+    @knowledge << (string).to_s
+  end  
+  
+  def knowledge
+    @knowledge
+  end
 
 end
